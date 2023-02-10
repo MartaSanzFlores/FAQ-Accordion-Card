e966fa52421394fb8b0600ba4c32129c37b54ed6
MartaSanzFlores <martasf22@gmail.com>
2023-02-10 14:59:16 +0100

    Improvement in js

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ var question = document.querySelectorAll(".q");
 question.forEach((q) => {
 	q.addEventListener("click", function() {
         q.lastChild.classList.toggle("answer2");
+        q.firstChild.classList.toggle("q2");
+        q.classList.toggle("q");
     })
 });
